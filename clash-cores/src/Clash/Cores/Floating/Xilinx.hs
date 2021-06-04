b7d189b2688138677f2ae5d4a8c68649aae7db06
Peter Lebbing <peter@digitalbrains.com>
2021-06-04 17:05:35 +0200

    fixup! New: Clash.Cores.Floating.Xilinx

--- clash-cores/src/Clash/Cores/Floating/Xilinx.hs
+++ clash-cores/src/Clash/Cores/Floating/Xilinx.hs
@@ -1,3 +1,37 @@
+{-|
+Copyright  :  (C) 2021,      QBayLogic B.V.,
+License    :  BSD2 (see the file LICENSE)
+Maintainer :  QBayLogic B.V. <devops@qbaylogic.com>
+
+Support for the [Xilinx Floating-Point LogiCORE IP v7.1](https://www.xilinx.com/support/documentation/ip_documentation/floating_point/v7_1/pg060-floating-point.pdf).
+
+The functions in this module make it possible to use the Xilinx IP in Clash.
+Simulation produces bit-identical results to synthesis, and compilation will
+instantiate the Xilinx IP. Clash will output a TCL script named
+@floating_point@/.../@.tcl@ which needs to be executed in the Vivado project to
+create the proper entity.
+
+Most functions allow customization of the Xilinx IP. Valid combinations will
+need to be gleaned from, e.g., the Vivado wizard (IP Catalog -> Floating-point).
+All IP instantiated by this module always have the following properties:
+* Single precision
+* Non-blocking
+* No reset input
+* No optional output fields, no @TLAST@, no @TUSER@.
+* @TVALID@ on the inputs is always asserted, @TVALID@ on the output is ignored.
+Note that it would appear the Xilinx IP does not use these signals anyway.
+
+The latency of the IP is set through the delay argument of the 'DSignal'. Other
+customization is done through the 'FloatingConfig' argument of customizable
+functions.
+
+De-asserting the 'Enable' signal of a function will stall the whole pipeline.
+
+The Xilinx IP does not support calculations with subnormal numbers. All
+subnormal numbers are rounded to zero on both input and output. Note that this
+introduces a slight bias as the larger subnormal numbers are closer to the
+smallest normal number, but they are rounded to zero nonetheless!
+-}
 module Clash.Cores.Floating.Xilinx
   ( absFloat
   , addFloat
