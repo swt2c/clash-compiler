5456af8651d7d0daf9d9fd4775a781ae946cab3a
Peter Lebbing <peter@digitalbrains.com>
2022-02-09 16:27:47 +0100

    fixup! [WIP] Test bench for `asyncFifoSynchronizer`
    [skip ci]

--- clash-prelude/tests/Clash/Tests/AsyncFIFOSynchronizer.hs
+++ clash-prelude/tests/Clash/Tests/AsyncFIFOSynchronizer.hs
@@ -24,15 +24,51 @@ sync1Samples = ((inputRInc, inputWDataM), out)
  where
   out = asyncFIFOSynchronizer d4 clockGen clockGen resetGen resetGen enableGen
           enableGen inputRInc inputWDataM
-  inputRInc = fromList $ P.replicate 4 False <> P.replicate 4 True <> P.replicate 4 False <> P.replicate 4 True <> P.repeat False
-  inputWDataM = fromList $ Nothing : P.map Just [1 .. 4] <> P.replicate 8 Nothing
+  inputRInc = fromList $ P.replicate 5 False <> P.replicate 4 True <> P.replicate 4 False <> P.replicate 4 True <> P.repeat False
+  inputWDataM = fromList $ Nothing : P.map Just [1 .. 4] <> P.repeat Nothing
 
-sync1Slow :: Signal Slow (Bool, (Int, Bool))
-sync1Slow = bundle (inputRInc, bundle (rdata, rempty))
+sync1R :: Signal Slow (Bool, (Int, Bool))
+sync1R = bundle (inputRInc, bundle (rdata, rempty))
  where
   ((inputRInc, _), (rdata, rempty, _)) = sync1Samples
 
-sync1System :: Signal System (Maybe Int, Bool)
-sync1System = bundle (inputWDataM, wfull)
+sync1W :: Signal System (Maybe Int, Bool)
+sync1W = bundle (inputWDataM, wfull)
  where
   ((_, inputWDataM), (_, _, wfull)) = sync1Samples
+
+sync2Samples :: ( ( Signal System Bool
+                  , Signal Slow (Maybe Int)
+                  )
+                , ( Signal System Int
+                  , Signal System Bool
+                  , Signal Slow Bool)
+                )
+sync2Samples = ((inputRInc, inputWDataM), out)
+ where
+  out = asyncFIFOSynchronizer d4 clockGen clockGen resetGen resetGen enableGen
+          enableGen inputRInc inputWDataM
+  inputRInc = fromList $ P.replicate 10 False <> P.replicate 4 True <> P.replicate 8 False <> P.replicate 4 True <> P.repeat False
+  inputWDataM = fromList $ Nothing : P.map Just [1 .. 4] <> P.repeat Nothing
+
+sync2R :: Signal System (Bool, (Int, Bool))
+sync2R = bundle (inputRInc, bundle (rdata, rempty))
+ where
+  ((inputRInc, _), (rdata, rempty, _)) = sync2Samples
+
+sync2W :: Signal Slow (Maybe Int, Bool)
+sync2W = bundle (inputWDataM, wfull)
+ where
+  ((_, inputWDataM), (_, _, wfull)) = sync2Samples
+
+printS1R :: IO ()
+printS1R = P.mapM_ (putStrLn . showX) $ sampleN 30 sync1R
+
+printS1W :: IO ()
+printS1W = P.mapM_ (putStrLn . showX) $ sampleN 30 sync1W
+
+printS2R :: IO ()
+printS2R = P.mapM_ (putStrLn . showX) $ sampleN 30 sync2R
+
+printS2W :: IO ()
+printS2W = P.mapM_ (putStrLn . showX) $ sampleN 30 sync2W
